194fec525b05963f6a8eb496d136b919658f9797
pzhou11 <pzhou11@users.noreply.github.com>
2017-02-28 09:35:02 -0800

    Update best_hospitals.sql

--- exercise_1/investigations/best_hospitals.sql
+++ exercise_1/investigations/best_hospitals.sql
@@ -1,6 +1,6 @@
-drop table hcahps_detail_best_filter;
+drop table best_hcahps_detail_filter;
 
-create table hcahps_detail_best_filter as
+create table best_hcahps_detail_filter as
 select
   provider_id,
   measure_id,
@@ -14,9 +14,9 @@ where number_of_completed_surveys>=300 and
 
 
 
-drop table complications_best_filter;
+drop table best_complications_filter;
 
-create table complications_best_filter as
+create table best_complications_filter as
 select
   provider_id,
   measure_id,
@@ -30,21 +30,21 @@ where denominator>=100 and
 
 
 
-drop table complications_best_filter2;
+drop table best_complications_filter2;
 
-create table complications_best_filter2 as
+create table best_complications_filter2 as
 select
   provider_id,
   count(provider_id) as count
-from complications_best_filter
+from best_complications_filter
 group by provider_id
-having count>=9
+having count>=7
 ;
 
 
-drop table readmission_best_filter;
+drop table best_readmission_filter;
 
-create table readmission_best_filter as
+create table best_readmission_filter as
 select
   provider_id,
   measure_id,
@@ -52,65 +52,63 @@ select
   denominator,
   score
 from readmissions_and_deaths
-where denominator>=100 and
+where denominator>=50 and
   denominator not like 'Not%'
 ;
 
 
-drop table readmission_best_filter2;
+drop table best_readmission_filter2;
 
-create table readmission_best_filter2 as
+create table best_readmission_filter2 as
 select
   provider_id,
   count(provider_id) as count
-from readmission_best_filter
+from best_readmission_filter
 group by provider_id
-having count>=12
+having count>=10
 ;
 
 
-drop table hcahps_summary_filter_final;
+drop table best_hcahps_summary_filter_final;
 
-create table hcahps_summary_filter_final as 
+create table best_hcahps_summary_filter_final as 
 select
   hcahps_summary.provider_id,
   hcahps_summary.overall_performance
 from hcahps_summary 
-inner join hcahps_detail_best_filter 
-  on hcahps_summary.provider_id = hcahps_detail_best_filter.provider_id
-inner join complications_best_filter2
-  on hcahps_summary.provider_id = complications_best_filter2.provider_id
-inner join readmission_best_filter2
-  on hcahps_summary.provider_id = readmission_best_filter2.provider_id
+inner join best_hcahps_detail_filter 
+  on hcahps_summary.provider_id = best_hcahps_detail_filter.provider_id
+inner join best_complications_filter2
+  on hcahps_summary.provider_id = best_complications_filter2.provider_id
+inner join best_readmission_filter2
+  on hcahps_summary.provider_id = best_readmission_filter2.provider_id
 ;
 
 
-drop table complications_best_filter_final;
+drop table best_complications_filter_final;
 
-create table complications_best_filter_final as 
+create table best_complications_filter_final as 
 select
-  complications_best_filter.provider_id,
-  sum(case when compared_to_national='Better than the National Rate' then 1 else 0 end) -
-  sum(case when compared_to_national='Worse than the National Rate' then 1 else 0 end) as net
-from complications_best_filter
-inner join hcahps_summary_filter_final
-  on complications_best_filter.provider_id = hcahps_summary_filter_final.provider_id
-group by complications_best_filter.provider_id
+  best_complications_filter.provider_id,
+  sum(case when compared_to_national='Better than the National Rate' then 1 else 0 end) as best
+from best_complications_filter
+inner join best_hcahps_summary_filter_final
+  on best_complications_filter.provider_id = best_hcahps_summary_filter_final.provider_id
+group by best_complications_filter.provider_id
 ;
 
 
 
-drop table readmission_best_filter_final;
+drop table best_readmission_filter_final;
 
-create table readmission_best_filter_final as 
+create table best_readmission_filter_final as 
 select
-  readmission_best_filter.provider_id,
-  sum(case when compared_to_national='Better than the National Rate' then 1 else 0 end) -
-  sum(case when compared_to_national='Worse than the National Rate' then 1 else 0 end) as net
-from readmission_best_filter
-inner join hcahps_summary_filter_final
-  on readmission_best_filter.provider_id = hcahps_summary_filter_final.provider_id
-group by readmission_best_filter.provider_id
+  best_readmission_filter.provider_id,
+  sum(case when compared_to_national='Better than the National Rate' then 1 else 0 end) as best
+from best_readmission_filter
+inner join best_hcahps_summary_filter_final
+  on best_readmission_filter.provider_id = best_hcahps_summary_filter_final.provider_id
+group by best_readmission_filter.provider_id
 ;
 
 
@@ -119,15 +117,15 @@ drop table best_hospital1;
 
 create table best_hospital1 as 
 select
-  hcahps_summary_filter_final.provider_id,
-  hcahps_summary_filter_final.overall_performance,
-  readmission_best_filter_final.net as readmission_score,
-  complications_best_filter_final.net as complications_score
-from hcahps_summary_filter_final
-inner join readmission_best_filter_final
-  on hcahps_summary_filter_final.provider_id = readmission_best_filter_final.provider_id
-inner join complications_best_filter_final
-  on hcahps_summary_filter_final.provider_id = complications_best_filter_final.provider_id
+  best_hcahps_summary_filter_final.provider_id,
+  best_hcahps_summary_filter_final.overall_performance,
+  best_readmission_filter_final.best as readmission_score,
+  best_complications_filter_final.best as complications_score
+from best_hcahps_summary_filter_final
+inner join best_readmission_filter_final
+  on best_hcahps_summary_filter_final.provider_id = best_readmission_filter_final.provider_id
+inner join best_complications_filter_final
+  on best_hcahps_summary_filter_final.provider_id = best_complications_filter_final.provider_id
 ;
 
 
@@ -138,11 +136,16 @@ create table best_hospital_final as
 SELECT 
   best_hospital1.provider_id,
   hospital_info.hospital_name,
-  ((RANK() OVER (ORDER BY best_hospital1.overall_performance DESC)) +
+  round(((RANK() OVER (ORDER BY best_hospital1.overall_performance DESC)) +
   (RANK() OVER (ORDER BY best_hospital1.readmission_score DESC)) +
-  (RANK() OVER (ORDER BY best_hospital1.complications_score DESC))) / 3 as score
+  (RANK() OVER (ORDER BY best_hospital1.complications_score DESC))) / 3,1) as overall_score,
+  (RANK() OVER (ORDER BY best_hospital1.overall_performance DESC)) as HCAHPS_rank,
+  (RANK() OVER (ORDER BY best_hospital1.readmission_score DESC)) as readmission_rank,
+  (RANK() OVER (ORDER BY best_hospital1.complications_score DESC)) as complications_rank
 from best_hospital1
 inner join hospital_info
   on best_hospital1.provider_id = hospital_info.provider_id
-order by score asc
+order by overall_score asc
 ;
+
+select * from best_hospital_final limit 10;
